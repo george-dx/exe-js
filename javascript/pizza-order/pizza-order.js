deb0b09ba9c9921f2f9cb01a489a76c4509580e8
george-dx <georgedaianx@gmail.com>
2024-11-06 09:40:04 +0100

    Calculate price without recursion

---
+++ javascript/pizza-order/pizza-order.js
@@ -0,0 +1,53 @@
+/// <reference path="./global.d.ts" />
+//
+// @ts-check
+
+/**
+ * Determine the price of the pizza given the pizza and optional extras
+ *
+ * @param {Pizza} pizza name of the pizza to be made
+ * @param {Extra[]} extras list of extras
+ *
+ * @returns {number} the price of the pizza
+ */
+export function pizzaPrice(pizza, ...extras) {
+  let price = 0
+  switch (pizza) {
+    case "Margherita":
+      price = 7;
+      break;
+    case "Caprese":
+      price = 9;
+      break;
+    case "Formaggio":
+      price = 10;
+      break;
+  }
+  extras.map(
+    extra => {
+      if (extra === 'ExtraSauce') price += 1; else price += 2;
+    }
+  )
+  return price;
+}
+
+/**
+ * Calculate the price of the total order, given individual orders
+ *
+ * (HINT: For this exercise, you can take a look at the supplied "global.d.ts" file
+ * for a more info about the type definitions used)
+ *
+ * @param {PizzaOrder[]} pizzaOrders a list of pizza orders
+ * @returns {number} the price of the total order
+ */
+export function orderPrice(pizzaOrders) {
+  let totalPrice = 0;
+  if (pizzaOrders.length > 0) {
+    for (var order of pizzaOrders) {
+      totalPrice += pizzaPrice(order.pizza, ...order.extras)
+    }
+    return totalPrice;
+  } else {
+    return 0;
+  }
+}
