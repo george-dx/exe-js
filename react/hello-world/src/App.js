38a86437d2f0a7c8e34fa2adc4381e20682216a9
george-dx <georgedaianx@gmail.com>
2025-01-28 11:36:07 +0100

    Scale up with reducer and context

--- react/hello-world/src/App.js
+++ react/hello-world/src/App.js
@@ -1,42 +1,77 @@
-import Heading from "./Heading.js";
-import Section from "./Section.js";
+import { useReducer } from "react";
+import AddTask from "./AddTask.js";
+import TaskList from "./TaskList.js";
+import { TasksContext, TasksDispatchContext } from "./TaskContext.js";
 
-export default function ProfilePage() {
+export default function TaskApp() {
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+
+  function handleAddTask(text) {
+    dispatch({
+      type: "added",
+      id: nextId++,
+      text: text,
+    });
+  }
+
+  function handleChangeTask(task) {
+    dispatch({
+      type: "changed",
+      task: task,
+    });
+  }
+
+  function handleDeleteTask(taskId) {
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
+  }
+
   return (
-    <Section>
-      <Heading>My Profile</Heading>
-      <Post title="Hello traveller!" body="Read about my adventures." />
-      <AllPosts />
-    </Section>
+    <TasksContext.Provider value={tasks}>
+      <TasksDispatchContext.Provider value={dispatch}>
+        <h1>Day off in Kyoto</h1>
+        <AddTask />
+        <TaskList />
+      </TasksDispatchContext.Provider>
+    </TasksContext.Provider>
   );
 }
 
-function AllPosts() {
-  return (
-    <Section>
-      <Heading>Posts</Heading>
-      <RecentPosts />
-    </Section>
-  );
+function tasksReducer(tasks, action) {
+  switch (action.type) {
+    case "added": {
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case "changed": {
+      return tasks.map((t) => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    }
+    case "deleted": {
+      return tasks.filter((t) => t.id !== action.id);
+    }
+    default: {
+      throw Error("Unknown action: " + action.type);
+    }
+  }
 }
 
-function RecentPosts() {
-  return (
-    <Section>
-      <Heading>Recent Posts</Heading>
-      <Post title="Flavors of Lisbon" body="...those pastéis de nata!" />
-      <Post title="Buenos Aires in the rhythm of tango" body="I loved it!" />
-    </Section>
-  );
-}
-
-function Post({ title, body }) {
-  return (
-    <Section isFancy={true}>
-      <Heading>{title}</Heading>
-      <p>
-        <i>{body}</i>
-      </p>
-    </Section>
-  );
-}
+let nextId = 3;
+const initialTasks = [
+  { id: 0, text: "Philosopher’s Path", done: true },
+  { id: 1, text: "Visit the temple", done: false },
+  { id: 2, text: "Drink matcha", done: false },
+];
