2c63fb5c262c1bbd095f249a48c377a5efcaea0a
george-dx <georgedaianx@gmail.com>
2025-01-29 11:14:44 +0100

    Synchronize with effects

--- react/hello-world/src/App.js
+++ react/hello-world/src/App.js
@@ -1,5 +1,22 @@
-import StopWatch from "./StopWatch";
+import { useState } from "react";
+import VideoPlayer from "./VideoPlayer";
+import ChatRoom from "./ChatRoom";
 
 export default function App() {
-  return <StopWatch />;
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [text, setText] = useState("");
+
+  return (
+    <>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={() => setIsPlaying(!isPlaying)}>
+        {isPlaying ? "Pause" : "Play"}
+      </button>
+      <VideoPlayer
+        isPlaying={isPlaying}
+        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+      />
+      <ChatRoom />
+    </>
+  );
 }
